1e54c7aa2ddaa95232e52d15e06f6bdf9f6d2d0c
eespinor <edgard.espinoza.rivas@gmail.com>
2023-08-11 14:13:36 -0500

    first commit

---
+++ app/src/main/java/com/eespinor/foregroundservices/RunningApp.kt
@@ -0,0 +1,21 @@
+package com.eespinor.foregroundservices
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class RunningApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val channel = NotificationChannel(
+            "running_channel",
+            "running notification",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
